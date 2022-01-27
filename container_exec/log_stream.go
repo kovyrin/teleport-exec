28c6e1e36c4d83cf6cb77ea1639e559ca2d826cd
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2022-01-26 21:22:08 -0500

    Introduce the EOF flag

--- container_exec/log_stream.go
+++ container_exec/log_stream.go
@@ -23,30 +23,15 @@ func NewLogStream(file_name string) *LogStream {
 	}
 }
 
-func (l *LogStream) MoreBytes(output []byte) (read_bytes int) {
-	// Get current position
-	current_pos, err := l.reader.Seek(0, io.SeekCurrent)
+func (l *LogStream) MoreBytes(output []byte) (read_bytes int, eof bool) {
+	read_bytes, err := l.reader.Read(output)
+	if err == io.EOF {
+		return read_bytes, true
+	}
 	if err != nil {
-		log.Fatalf("Failed to find current file position for '%s': %v", l.fileName, err)
-	}
-
-	// Get file size
-	stat, err := os.Stat(l.fileName)
-	if err != nil {
-		log.Fatalf("Failed to check file status for file '%s': %v", l.fileName, err)
-	}
-
-	// If there is nothing to read, just return an eof
-	if stat.Size() == current_pos {
-		return 0
-	}
-
-	// Otherwise, read up to max_bytes of data
-	read_bytes, err = l.reader.Read(output)
-	if err != nil && err != io.EOF {
 		log.Fatalf("Failed to read data from file '%s': %v", l.fileName, err)
 	}
-	return read_bytes
+	return read_bytes, false
 }
 
 func (log *LogStream) Close() error {
