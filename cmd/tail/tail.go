2b748dd1c5c7812b7ebd8b07e6650cdd2e08f1c6
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2022-01-29 10:52:56 -0500

    Add signal handling

--- cmd/tail/tail.go
+++ cmd/tail/tail.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/signal"
 	"teleport-exec/filestream"
 	"time"
 )
@@ -21,10 +22,12 @@ func main() {
 	timeout_ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// Print a message when the timeout is reached
+	// Stop the stream when cancelled via Ctrl+C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 	go func() {
-		<-timeout_ctx.Done()
-		log.Println("Timeout reached")
+		<-c
+		cancel()
 	}()
 
 	// Start a new stream from the file
